bfaafe35b16240793c9d83801a91ac6e5a3f6904
Blessing Abayomi <blessingabayomi0204@gmail.com>
2024-06-26 02:42:05 +0100

    project file uploads v2.0

---
+++ app.js
@@ -0,0 +1,26 @@
+/* ---------- RANDOM NUMBER GENERATOR ---------- */
+
+const myBtn = document.getElementById("myBtn");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+
+
+const min = 1;
+const max = 9;
+
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
+
+myBtn.addEventListener("click", generateNumber);
+
+function generateNumber() {
+    randomNumber1 = Math.floor(Math.random() * max) + min;
+    randomNumber2 = Math.floor(Math.random() * max) + min;
+    randomNumber3 = Math.floor(Math.random() * max) + min;
+
+    label1.textContent = randomNumber1;
+    label2.textContent = randomNumber2;
+    label3.textContent = randomNumber3;
+}
